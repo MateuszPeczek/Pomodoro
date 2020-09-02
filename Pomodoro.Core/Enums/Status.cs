f9a3312db8b53d2de7104337ed487315c30e93ca
Mateusz <peczek.mateusz@gmail.com>
2020-09-02 12:32:54 +0200

    Enums and enum helpers

---
+++ Pomodoro.Core/Enums/Status.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Pomodoro.Core.Enums
+{
+    public enum Status
+    {
+        [Description(@"Waiting for start...")]
+        Waiting,
+        [Description(@"Take a break!")]
+        Break,
+        [Description(@"Focus!")]
+        Focus
+    }
+}
---
+++ Pomodoro.Core/Helpers/EnumHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Pomodoro.Core.Helpers
+{
+    public static class EnumHelpers
+    {
+        public static string GetEnumDescription(this Enum @enum)
+        {
+            var fieldInfo = @enum.GetType().GetField(@enum.ToString());
+            var descriptionAttribute = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false).Select(a => (DescriptionAttribute)a).SingleOrDefault();
+
+            return descriptionAttribute.Description;
+        }
+    }
+}
